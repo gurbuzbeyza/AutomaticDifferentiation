ba24a59b398b85045affe722a64769babb601374
beyza <beygrbz@gmail.com>
2018-02-18 18:55:11 +0300

    all variables are executed

--- Input.py
+++ Input.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from math import *
+import numpy as np
 
 NO_OPERATION = 'no operation'
 
@@ -37,7 +39,7 @@ def take_input():
 			if(l[2] in operations):
 				variables.append([l[0],Operation(l[2]),l[3:]])	#[varName, operName, [variables]]
 			else:
-				variables.append([l[0],NO_OPERATION,l[3:]])	#[varName, operName, [variables]]
+				variables.append([l[0],NO_OPERATION,l[2:]])	#[varName, operName, [variables]]
 	i = input_vals[0].split()
 	n = len(i)
 	for x in i:
@@ -49,17 +51,37 @@ def take_input():
 		inputs.value_list.append(i)
 	return variables, inputs, output
 
+# def choose_operation(op, v):
+#     return {
+#         Operation.Mult: exec (v[0] + ' = ' + v[2][0] + ' * ' + v[2][1]),
+#         Operation.Sin: exec (v[0] + ' = ' + 'sin(' + v[2][1] + ')'),
+#         Operation.Cos: exec (v[0] + ' = ' + 'cos(' + v[2][1] + ')'),
+#         Operation.Log: exec (v[0] + ' = ' + 'log(' + v[2][1] + ')')
+#     }[op]
+
 def __main__():
 	variables, inputs, output = take_input()
 	print(variables)
 	print(inputs)
 	print(output)
+	jacobian = [][]
 
 	for i in inputs.value_list:
 		for j in range(len(inputs.vars)):
 			exec (inputs.vars[j] + ' = ' + i[j])
 			print (inputs.vars[j])
 			exec ('print (' + inputs.vars[j] + ')')
-			for v in variables
+		for v in variables:
+			if v[1] == Operation.Mult:
+				exec (v[0] + ' = ' + v[2][0] + ' * ' + v[2][1])
+			elif v[1] == Operation.Sin:
+				exec (v[0] + ' = ' + 'sin(' + v[2][0] + ')')
+			elif v[1] == Operation.Cos:
+				exec (v[0] + ' = ' + 'cos(' + v[2][0] + ')')
+			else:
+				exec (v[0] + ' = ' + v[2][0])
+			# choose_operation(v[1], v)
+			print (v[0])
+			exec ('print (' + v[0] + ')')
 
 __main__()
