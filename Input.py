68e716f8ccc26048309b91777b8583ada3585dd9
esra-aydemir <h.esraaydemir@hotmail.com>
2018-02-18 20:55:14 +0300

    forward derivatives started

--- Input.py
+++ Input.py
@@ -59,12 +59,21 @@ def take_input():
 #         Operation.Log: exec (v[0] + ' = ' + 'log(' + v[2][1] + ')')
 #     }[op]
 
+def isScalar(n):
+	try:
+		float(n)
+		return True
+	except ValueError:
+		return False
+
 def __main__():
 	variables, inputs, output = take_input()
 	print(variables)
 	print(inputs)
 	print(output)
-	jacobian = [][]
+	#jacobian = [][]
+	jacobian = []
+	val_ders = []	#list of: ['var_name','value',[['derivativeToX',val],'derivativeToY',val],...]] #leafs #forwardPass
 
 	for i in inputs.value_list:
 		for j in range(len(inputs.vars)):
@@ -72,16 +81,47 @@ def __main__():
 			print (inputs.vars[j])
 			exec ('print (' + inputs.vars[j] + ')')
 		for v in variables:
+			valder = [0,0,[]]#placeholder	#['var_name','value','derivative']
+			#!!note:!! execs below might be insufficient execute more(? :D) if errors occur
+
 			if v[1] == Operation.Mult:
-				exec (v[0] + ' = ' + v[2][0] + ' * ' + v[2][1])
+				valder[0]=v[0]
+				valder[1]=eval(v[2][0] + ' * ' + v[2][1]);
+				exec (v[0] + ' = ' + v[2][0] + ' * ' + v[2][1])	#exec valder[0] = valder[1]
+				if not isScalar(v[2][0]) and not isScalar(v[2][1]):
+					valder[2].append([v[2][0],eval(v[2][1])])
+					valder[2].append([v[2][1],eval(v[2][0])])
+				elif not isScalar(v[2][0]):
+					valder[2].append([v[2][0],eval(v[2][1])])
+				elif not isScalar(v[2][1]):
+					valder[2].append([v[2][1],eval(v[2][0])])
+				else:	#not expected
+					print("multiplication of 2 scalars given! should i handle it?")
 			elif v[1] == Operation.Sin:
-				exec (v[0] + ' = ' + 'sin(' + v[2][0] + ')')
+				valder[0]=v[0]
+				valder[1] = eval('sin(' + v[2][0] + ')')
+				exec (v[0] + ' = ' + 'sin(' + v[2][0] + ')')	#exec valder[0] = valder[1] i.e. t_1 = sin(t_0)
+				valder[2].append([v[2][0],eval('cos(' + v[2][0] + ')')])
 			elif v[1] == Operation.Cos:
-				exec (v[0] + ' = ' + 'cos(' + v[2][0] + ')')
-			else:
+				valder[0]=v[0]
+				valder[1] = eval('cos(' + v[2][0] + ')')
+				exec (v[0] + ' = ' + 'cos(' + v[2][0] + ')')	#exec valder[0] = valder[1] 
+				valder[2].append([v[2][0],eval('-sin(' + v[2][0] + ')')])
+			elif v[1] ==NO_OPERATION:
+				valder[0] = v[0]
+				valder[1] = eval(v[2][0])
+				valder[2].append([v[2][0],1])
 				exec (v[0] + ' = ' + v[2][0])
+			else:
+				print("i see operations you did not handle!! like: "+v[1])
+			val_ders.append(valder)
 			# choose_operation(v[1], v)
 			print (v[0])
 			exec ('print (' + v[0] + ')')
+
+
+			print("val_ders:")
+			for i in val_ders:
+				print(i)
 
 __main__()
