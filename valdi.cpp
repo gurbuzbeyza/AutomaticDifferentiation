b42594a6c9e545c0188ed4eb8f25d25d6821407f
esra-aydemir <h.esraaydemir@hotmail.com>
2018-03-10 01:37:56 +0300

    python implementation of functions moved to cpp

--- valdi.cpp
+++ valdi.cpp
@@ -4,7 +4,10 @@
 #include <tuple>
 #include <math.h>
 //http://www.cplusplus.com/reference/tuple/tuple/
-
+//implemented only operations between valders. TODO expand for valder, float operations -> tum floatlari valdere cast edeceksek gerek de yok mu acaba???
+// TODO add graph field to valder ? -> keys of AddDer graph yerine gecer mi??? niye lazim ki graph:D
+//TODO remove name field?
+//TODO implement = overloading to pass by reference and to assign floats to valders at initialization.
 using namespace std;
 
 class Valder {
@@ -25,6 +28,7 @@ class Valder {
                 }
                 void DisplayDerValues()
                 {
+                    cout<<"displaying ders:"<<endl;
                         for( unsigned int i = 0; i < ders.size(); i++ )
                         {
                             cout<<"derivative to "<<std::get<0>(ders[i]).getName()<< " is: " << std::get<1>(ders[i]) << endl;
@@ -50,22 +54,40 @@ class Valder {
                 // Overload + operator to add two Valder objects.
                 Valder operator+(Valder b) {
                     Valder vd;
-                    vd.val = this->val + b.val;
+                    vd.setVal(this->val + b.getVal());
                     vd.AddDer(b,1);
                     vd.AddDer(*this,1);
                     vd.setName("deleteNameField");
                     return vd;
                }
+               //subtraction
+               Valder operator-(Valder b){
+                    Valder vd;
+                    vd.setVal(this->val-b.getVal());
+                    vd.AddDer(b,-1);
+                    vd.AddDer(*this,1);
+                    vd.setName("subtractionresult");
+                    return vd;
+                }
 
                // Overload * operator to multiply two Valder objects.
                 Valder operator*(Valder b) {
                     Valder vd;
-                    vd.val = this->val * b.val;
+                    vd.setVal(this->val * b.getVal());
                     vd.AddDer(b,this->val);
                     vd.AddDer(*this,b.getVal());
                     vd.setName("deleteNameField");
                     return vd;
                }
+                //division
+                Valder operator/(Valder b){
+                    Valder v;
+                    v.setVal(this->val / b.getVal());
+                    v.AddDer(b,(-this->val)/b.getVal()*b.getVal());
+                    v.AddDer(*this,1/b.getVal());
+                    v.setName("div result");
+                    return v;
+                }         
 
         private:
                 std::vector<tuple<Valder,float>> ders;
@@ -73,6 +95,25 @@ class Valder {
                 float val;
 
 };
+
+Valder pow(Valder var,Valder p){
+    Valder v;
+    v.setName("powresult");
+    v.setVal(pow(var.getVal(),p.getVal()));
+    v.AddDer(var,p.getVal()*(pow(var.getVal(),p.getVal()-1)));
+    v.AddDer(p,pow(var.getVal(),p.getVal())*log(var.getVal()));
+    return v;   
+}
+
+Valder sqrt(Valder var){
+    Valder v;
+    v.setName("sqrtresult");
+    v.setVal(sqrt(var.getVal()));
+    v.AddDer(var,1/(2*sqrt(var.getVal())));
+    return v;
+}
+
+
 
 Valder sin(Valder var) {
     Valder v;
@@ -92,20 +133,81 @@ Valder cos(Valder var) {
     return v;
 }
 
+Valder tan(Valder var){
+    Valder v;
+    v.setName("tanresult");
+    v.setVal(tan(var.getVal()));
+    v.AddDer(var,1/(pow(cos(var.getVal()),2)));
+    return v;
+}
+
+Valder asin(Valder var){    
+    Valder v;
+    v.setName("asinresult");
+    v.setVal(asin(var.getVal()));
+    v.AddDer(var,1/sqrt(1-pow(var.getVal(),2)));
+    return v;
+}
+Valder acos(Valder var){    
+    Valder v;
+    v.setName("acosresult");
+    v.setVal(acos(var.getVal()));
+    v.AddDer(var,(-1)/sqrt(1-pow(var.getVal(),2)));
+    return v;
+}
+
+Valder atan(Valder var){    
+    Valder v;
+    v.setName("atanresult");
+    v.setVal(atan(var.getVal()));
+    v.AddDer(var,1/pow(var.getVal(),2)+1);
+    return v;
+}
+
+Valder exp(Valder var){
+    Valder v;
+    v.setName("expresult");
+    v.setVal(exp(var.getVal()));
+    v.AddDer(var,exp(var.getVal()));
+}
+
+Valder log(Valder var){
+    Valder v;
+    v.setName("logresult");
+    v.setVal(log(var.getVal()));
+    v.AddDer(var,1/var.getVal());
+    return v;
+}
+
+/*//template
+
+Valder Z(Valder var){    
+    Valder v;
+    v.setName("Zresult");
+    v.setVal(Z(var.getVal()));
+    v.AddDer(var,ZZZZ);
+    return v;
+}*/
+
 
 int main() {
+    
+    /// just try some stuff
     Valder a;
     Valder b;
     Valder result;
     a.setName("a");
     b.setName("b");
-    a.setVal(1);
+    a.setVal(2.7);
+    a.AddDer(b,3);
     b.setVal(2);
     cout<<"a: "<<endl;
     a.DisplayDerValues();
-    result =sin(a);
-    cout<<"result: "<<endl;
-    result.DisplayDerValues();
+    result.setVal(23);
+    result =a;
+    a=log(result)/b;
+    cout<<"a again: "<<a.getVal()<<endl;
+    a.DisplayDerValues();
 
     return 0;
 }
